2a3596097fa5a85b32dceb6eedc6d8236d7b7b00
Joel Okoniha <109484326+okjoe360@users.noreply.github.com>
2025-04-21 08:56:15 -0600

    Update services.py

--- app/services.py
+++ app/services.py
@@ -1,4 +1,6 @@
 from .extensions import *
+from .user.models import User
+from flask import Flask, redirect, request, flash, send_from_directory
 
 def initializer_service(app):
     db.init_app(app)
@@ -8,3 +10,12 @@ def initializer_service(app):
     cors.init_app(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
     login_manager.init_app(app)
     jwt.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    @login_manager.unauthorized_handler
+    def unauthorized_callback():
+        flash("Login to continue", "danger")
+        return redirect('/login?next=' + request.path)
