8ac7fc7d43498a0c0ac6684acd9f193a52e6cf48
Joel Okoniha <109484326+okjoe360@users.noreply.github.com>
2025-04-21 08:50:35 -0600

    Created Initializer services.py

--- app/services.py
+++ app/services.py
@@ -1,1 +1,10 @@
+from .extensions import *
 
+def initializer_service(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
+    admin.init_app(app)#, name='Admin', template_mode='bootstrap3', index_view=CustomAdminIndexView())
+    bcrypt.init_app(app)
+    cors.init_app(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
+    login_manager.init_app(app)
+    jwt.init_app(app)
